8f82ce037baff58992a5a89cb6fc06e5968a1d62
Tien Phong <dauuphongg@gmail.com>
2024-06-02 16:43:03 +0700

    two

--- src/pages/Regpage.js
+++ src/pages/Regpage.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import RegistrationForm from './Form';
+import Dashboard from './Dashboard';
 
 function Regpage() {
     return (
         <div>
-            {/* Your content goes here */}
+            <section className="container">
+                <div className="row">
+                    <div className="col-md-6">
+                        <h1>Đăng ký xét tuyển</h1>
+                        <RegistrationForm/>
+                    </div>
+                    <div className="col-md-6">
+                        <Dashboard/>
+                    </div>
+                </div>
+            </section>
+            
         </div>
     );
 }
