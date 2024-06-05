da20d98853e7707dcdd29b24b3d80ad14896b783
Tien Phong <dauuphongg@gmail.com>
2024-06-05 13:35:42 +0700

    link

--- src/pages/404page.js
+++ src/pages/404page.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 const NotFoundPage = () => {
   return (
-    <div>
+    <div className='text-center row '>
+      <div className='col fs-5'>
       <h1>404 - Trang không tồn tại</h1>
       <p>Trang bạn đang tìm kiếm không tồn tại.</p>
+      </div>
     </div>
   );
 }
